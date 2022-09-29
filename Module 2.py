0f3a0770504dc26ed153bd708aef21e05245fb66
sorinchineata <sorinc.chineata@gmail.com>
2022-09-29 18:19:38 +0300

    Update Module 2.py

--- Module 2.py
+++ Module 2.py
@@ -3,7 +3,7 @@ import random #import random module
 
 count=0 #number of dictionaries in list
 dictlist = [] #define the dictionary
-for i in range(random.randint(2,2)): #define the random number of dictionaries
+for i in range(random.randint(2,3)): #define the random number of dictionaries
     size = random.randint(1,3)    #define the random dictionary size for every one of the dictionaries
     keys = random.sample(string.ascii_lowercase,size)  #define the random letters for every one of the dictionaries using the size defined above
     values = (random.randint(0,3) for j in range(size)) #define the random numbers for every one of the dicitonaries using the size defined above with values between 0 and 100
@@ -33,13 +33,17 @@ for i in range(0, count):
                         dictnum = j+1
                     char = key_1 + "_" + str(dictnum)
 
-            if double == 1:
-                onedict = {(char,maxvalue)}
-                #print(onedict)
+
+            if double == 0:
+                onedict = {(key_1, dict1[key_1])}
+                # print(onedict)
                 finaldict.update(onedict)
-            else: onedict = {(key_1,dict1[key_1])}
-            #print(onedict)
+
+            else:onedict = {(char,maxvalue)}
+                #print(onedict)
             finaldict.update(onedict)
+
+
 
 
 
