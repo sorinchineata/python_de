7aaac7086b2d6ceabca9c6ce29392bff35719d5e
sorinchineata <sorinc.chineata@gmail.com>
2023-03-13 15:47:44 +0200

    Updates

--- Module 2/Module 2.py
+++ Module 2/Module 2.py
@@ -3,16 +3,16 @@ import random #import random module
 
 count=0 #number of dictionaries in list
 dictlist = [] #define the dictionary
-for i in range(random.randint(2,4)): #define the random number of dictionaries
-    size = random.randint(1,4)    #define the random dictionary size for every one of the dictionaries - maximum is 26 because we have 26 small letters in the english alphabet
+for i in range(random.randint(2,10)): #define the random number of dictionaries
+    size = random.randint(1,26)    #define the random dictionary size for every one of the dictionaries - maximum is 26 because we have 26 small letters in the english alphabet
     keys = random.sample(string.ascii_lowercase,size)  #define the random letters for every one of the dictionaries using the size defined above
-    values = (random.randint(0,4) for j in range(size)) #define the random numbers for every one of the dicitonaries using the size defined above with values between 0 and 100
+    values = (random.randint(0,100 ) for j in range(size)) #define the random numbers for every one of the dicitonaries using the size defined above with values between 0 and 100
     oneDict = dict(zip(keys,values))  #create the dictionary from the two sequences keys and values
     dictlist.append(oneDict)     #add the dictionary created above to the list of dictionaries
     count = count + 1 #will be used to keep track of the number of dictionaries in the list
 
 print('List of random generated dictionaries:')
-print(dictlist)
+print(dictlist) 
 #print(count)
 
 
--- Module 4/Module 4 Refactor M2.py
+++ Module 4/Module 4 Refactor M2.py
@@ -1,14 +1,47 @@
-import sys
 import random
 import string
 
-#create function to generate random dictionary
-
-def generate_random_dictionary():
+def random_dictionary():
     i = 0
-    random_dictionary = {}
-    while i < random.randint(2, 10):
-        random_dictionary[random.choice(string.ascii_lowercase)] = random.randint(0, 100)
+    oneDict = {}
+    while i < random.randint(1,26):
+        oneDict [random.choice(string.ascii_lowercase)] = random.randint(0, 100)
         i += 1
-    return random_dictionary
+    return oneDict
+
+def dict_list():
+    i=0
+    dictlist = [] 
+    while i < random.randint(2,10):
+        dictlist.append(random_dictionary())
+        i += 1
+    return dictlist
+
+def common_dict(dictlist):
+    interdict = {}
+    for i in range(len(dictlist)):
+        for key,value in dictlist[i].items():
+            if key not in interdict.keys():
+                interdict[key] = [[value], [i+1]]
+            elif key in interdict.keys():
+                interdict[key][0].append(value)
+                interdict[key][1].append(i+1)
+    return interdict
+
+     
+def final_dictionary(dict):
+    finaldict = {}
+    for key, value in dict.items():
+        if len(value[0]) > 1:
+            max_val = max(value[0])
+            key = key + '_' + str(value[1][value[0].index(max_val)])
+            finaldict[key] = max_val
+        elif len(value[0]) == 1:
+            finaldict[key] = value[0][0]
+    return finaldict
+
+
+generated_list = dict_list()
+print ('List of random generated dictionaries: ' + str(generated_list)+ '\n')
+print ('Final dictionary:' + str(final_dictionary(common_dict(generated_list))))
 
